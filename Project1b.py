dbe97ad1843ac436dba95eb0e26274cedbe48b8e
ZichengH-Davis <ziihuang@ucdavis.edu>
2025-10-31 20:34:17 -0700

    Update Project1b.py

--- Project1b.py
+++ Project1b.py
@@ -4,19 +4,20 @@ f = open("PCAP1_1.pcap", "rb")
 pcap = dpkt.pcap.Reader(f)
 
 i = 0
-for timestamp, buf in pcap:
+for timestamp, data in pcap:
     i += 1
     if i != 112:
         continue  
 
-    eth = dpkt.ethernet.Ethernet(buf)
+    eth = dpkt.ethernet.Ethernet(data)
     ip = eth.data
     tcp = ip.data
     payload = tcp.data
 
-    text = payload.decode('utf-8', errors='replace')
+    text = payload.decode()
 
     print(text)
     break  
 
 f.close()
+
